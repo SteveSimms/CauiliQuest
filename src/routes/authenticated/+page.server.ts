92a07c08800c33ffd01b2eebd3b9b17f52418ee7
Steven Simms <simmsstev@gmail.com>
2023-09-16 23:29:54 -0700

    Discovered which method to call to change usr data

--- src/routes/authenticated/+page.server.ts
+++ src/routes/authenticated/+page.server.ts
@@ -1,7 +1,12 @@
 import { redirect } from '@sveltejs/kit'
+import { auth } from '$lib/server/auth.js'
 
 export async function load({ locals }) {
 	const { user } = await locals.auth.validateUser()
 	if (!user) throw redirect(302, '/login')
+	// console.log(auth.updateUserAttributes(user.userId, { username: 'Stephanos ' })) // todo[] call this function from form on frontend
 	return { user }
 }
+
+
+
