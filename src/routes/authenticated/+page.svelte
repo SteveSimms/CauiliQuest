c12cb17146cbe57ae789a0b57b745b67afba1326
Steven Simms <simmsstev@gmail.com>
2023-09-17 12:59:37 -0700

    Updating username in profile section

--- src/routes/authenticated/+page.svelte
+++ src/routes/authenticated/+page.svelte
@@ -1,8 +1,34 @@
 <script lang="ts">
+		import { superForm } from 'sveltekit-superforms/client'
+
 	export let data
+
+	const { form, errors, constraints, enhance } = superForm(data.form)
+
+	console.log('Updated Username',data.user.username)
 </script>
 
-<div class="p-4">
+<div class="card m-auto mt-16 max-w-md p-8">
 	<h1>Profile</h1>
 	<p class="mt-4 capitalize">Welcome, {data.user.username}!</p>
+<form method="POST" class="mt-8 space-y-8"  use:enhance>
+	<label class="label" for="username">
+		<span class="block">Username</span>
+		<input 
+				class="input" 
+				type="text" 
+				name="username" 
+				id="username" 
+				class:input-error={$errors.username}
+				aria-invalid={$errors.username ? 'true' : undefined}
+				bind:value={$form.username}
+				{...$constraints.username}
+				>
+
+	</label>
+
+
+<button class="btn variant-filled" type="submit">Save Username</button>
+</form>
+
 </div>
